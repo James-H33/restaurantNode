9491c5f95dcb4f19aacfe532df1ff6b01a1e2bb0
James Hall-Treworgy <James Hall-Treworgy>
2016-03-07 21:54:05 -0500

    Fixed styling inconsistencies

---
+++ public/javascript/landing.js
@@ -0,0 +1,8 @@
+var body = document.querySelector(".overlay");
+var heightIntr = window.outerHeight;
+
+function bodyHeightAdjust() {
+  body.style.height = heightIntr + "px";
+}
+
+bodyHeightAdjust();
